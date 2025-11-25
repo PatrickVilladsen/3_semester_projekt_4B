9bf419e83b9a78c9e0714c5c9fbed400d11e114d
PatrickVilladsen <patrickvilladsen0@gmail.com>
2025-11-25 11:29:03 +0100

    Tilføjet /data på hjemmesiden

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
 
 dummy_json = '{"Temperature":23, "Humidity": 10}'
 
+def read_humidity(json_data):
+    data = json.loads(json_data)
+    return data["Humidity"]
+
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template('index.html')
+
+@app.route("/data")
+def data():
+    humidity_data = read_humidity(dummy_json)
+    return render_template('data.html', humidity_data = humidity_data)
 
 if __name__ == "__main__":
     app.run()
