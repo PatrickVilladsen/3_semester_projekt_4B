4038e7f1e54b81f76addb52dd177ee1b4aa997de
PatrickVilladsen <patrickvilladsen0@gmail.com>
2025-11-25 11:14:04 +0100

    Ændret funktionnavn

--- test.py
+++ test.py
@@ -2,8 +2,8 @@ import json
 
 dummy_json = '{"Temperature":23, "Humidity":10}'
 
-def read_json(json_data):
+def read_humidity(json_data):
     data = json.loads(json_data)
     return data["Humidity"]
 
-print(read_json(dummy_json))
+print(read_humidity(dummy_json))
