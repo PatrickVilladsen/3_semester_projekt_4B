b575ce1d7bf2f4979abeb21674b751a84d5ac7f5
PatrickVilladsen <patrickvilladsen0@gmail.com>
2025-11-25 11:29:36 +0100

    deleted test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import json
-
-dummy_json = '{"Temperature":23, "Humidity":10}'
-
-def read_humidity(json_data):
-    data = json.loads(json_data)
-    return data["Humidity"]
-
-print(read_humidity(dummy_json))
