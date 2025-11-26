be782b9a5536b878cc59530d90f86af4c8ee677e
Luxarian <frednehm@gmail.com>
2025-11-26 11:51:38 +0100

    Hjemmeside

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request 
+from flask_socketio import SocketIO, emit 
+import requests 
+import datetime 
 
+#Create the flask appplication object 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return render_template("index.html")
+#Insert the api key from our own api
+api_key = '??????'
 
-if __name__ == "__main__":
-    app.run()
+@app.route("/")
+def test() : 
+    return render_template('index.html', utc_dt=datetime.datetime.utcnow())
+#app.run starts the flask server 
+if __name__ == "__main__": 
+    app.run(debug=True) 
+
+
+
+
+    
