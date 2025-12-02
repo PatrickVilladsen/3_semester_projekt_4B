ab42d3800aeabdc2ee66ef1cbbe4299fe6dcff31
Oliver Betting <oliver.betting2000@gmail.com>
2025-12-02 15:04:00 +0100

    WindowControl kan styre solonoid, buzee og motor

--- Windowfiles/windowControl.py
+++ Windowfiles/windowControl.py
@@ -1,7 +1,21 @@
 from stepper import Stepper
-from machine import Pin
+from machine import Pin, PWM
 from time import sleep
 
+
+
+
+Solinoid = Pin(12, Pin.OUT)
+
+buzzerpin = Pin(14, Pin.OUT)
+buzzer_pwm = PWM(buzzerpin)
+
+def buzzer(pwm_object, frequency, tone_duration, silence_duration):
+    pwm_object.duty(512)
+    pwm_object.freq(frequency)
+    sleep(tone_duration)
+    pwm_object.duty(0)
+    sleep(silence_duration)
 
 In1 = Pin(16, Pin.OUT)
 In2 = Pin(17, Pin.OUT)
@@ -19,10 +33,17 @@ def step(count):
     currentStep = currentStep + count
     s1.step(count)
     
+buzzer(buzzer_pwm,440,1,0.02)
+Solinoid.on()
+sleep(1)
+
+
+ 
 step(509)
 sleep(1)
 print(currentStep)
 step(-509)
 sleep(1)
 print(currentStep)
-
+Solinoid.off()
+sleep(1)
