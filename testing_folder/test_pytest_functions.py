c9c5fb8000902167b42bc7c8ead3e944fd36bc2a
PatrickVilladsen <patrickvilladsen0@gmail.com>
2025-12-02 13:05:01 +0100

    pytests works now

--- testing_folder/test_pytest_functions.py
+++ testing_folder/test_pytest_functions.py
@@ -1,10 +1,19 @@
 import pytest
 from functions_to_test import *
 
+def addition(a,b):
+    return a+b
+
 # Pytest - read_json()
 def test_read_json():
-    assert pytest.read_json("testing_json.json") == "{'key': 'value'}" #true
-    assert pytest.read_json("does_not_exist.json") == "{'key': 'value'}" #false
-    assert pytest.read_json("does_not_exist.json") == "[Errno 2] No such file or directory: 'does_not_exist.json'" #true
-    assert pytest.read_json("empty_json.json") == "{'key': 'value'}" #false
-    assert pytest.read_json("empty_json.json") == "Decoding error: file could be empty" #true
+    
+    assert read_json("testing_json.json") == {'key': 'value'} #true
+    #assert read_json("does_not_exist.json") == {'key': 'value'} #false
+    with pytest.raises(FileNotFoundError):
+        read_json("does_not_exist.json") #true
+    #with pytest.raises():
+     #   read_json("empty_json.json")
+    #assert read_json("empty_json.json") == {'key': 'value'} #false
+    assert read_json("empty_json.json") == "Decoding error: file could be empty" #true
+    
+    assert addition(1,1) == 2 #true
