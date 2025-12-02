1bb6906d57171ca48e672d62662d153d8d9a0d31
PatrickVilladsen <patrickvilladsen0@gmail.com>
2025-12-02 09:50:44 +0100

    added (py)test scenarios

--- test_pytest_app.py
+++ test_pytest_app.py
@@ -2,5 +2,9 @@ import pytest
 from app.py import *
 
 def test_read_json():
-    assert pytest.read_json("test_json.json") == "something" #true
+    assert pytest.read_json("test_json.json") == "key:value" #true
+    assert pytest.read_json("does_not_exist.json") == "key:value" #false
+    assert pytest.read_json("does_not_exist.json") == "[Errno 2] No such file or directory: 'does_not_exist.json'" #true
+    assert pytest.read_json("empty_json.json") == "key:value" #false
+    assert pytest.read_json("empty_json.json") == "Decoding error: file could be empty" #true
 
