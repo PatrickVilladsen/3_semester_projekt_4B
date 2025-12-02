2b020dfc1c4be952a57625bd1d69981e4f902425
PatrickVilladsen <patrickvilladsen0@gmail.com>
2025-12-02 10:16:05 +0100

    updated function

--- tests/pytest_functions.py
+++ tests/pytest_functions.py
@@ -1,10 +1,16 @@
 import json
 from pathlib import Path
 
-def read_json(json_fil: Path) -> dict:
+def read_json(json_file: Path) -> dict:
+    """Read from a given json-file.
+    Args:
+        json_fil(Path): given json-file.
+    Returns:
+        dict: json.load(json_file).
+    """
     try:
-        with open(json_fil) as json_file:
-            json_data = json.load(json_file)
+        with open(json_file) as data:
+            json_data = json.load(data)
             return json_data
     except json.decoder.JSONDecodeError:
         return "Decoding error: file could be empty"
